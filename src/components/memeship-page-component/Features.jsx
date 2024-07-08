3f35001e54aaf7882ddfd79c42e755c806ab96df
Dagaroth3exe <buzybeingflmaboyant@gmail.com>
2024-07-08 13:50:53 +0530

    Updates Subscription design

--- src/components/memeship-page-component/Features.jsx
+++ src/components/memeship-page-component/Features.jsx
@@ -1,3 +1,5 @@
+import { RefreshCw } from 'lucide-react';
+
 const Features = () => {
   return (
     <div className="grid place-items-center p-5 sm:p-20 w-full">
@@ -7,7 +9,9 @@ const Features = () => {
             key={index}
             className="shadow-deep flex justify-center items-center shadow-2 h-[200px] w-[200px] sm:h-[250px] sm:w-[250px] border-2 rounded-full bg-gradient-to-r from-[#3E1A78] to-[#F09AB2] hover:scale-110 cursor-pointer transition"
           >
-            <div className="h-[185px] w-[185px] sm:h-[235px] sm:w-[235px] rounded-full bg-white"></div>
+            <div className="h-[185px] w-[185px] sm:h-[235px] sm:w-[235px] rounded-full bg-white">
+            
+            </div>
           </div>
         ))}
       </section>
