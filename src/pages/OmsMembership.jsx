ce869d4a0a377bcd19d7cf97311cc6bbf10d75c9
Dagaroth3exe <buzybeingflmaboyant@gmail.com>
2024-07-15 12:29:01 +0530

    new subscription page

--- src/pages/OmsMembership.jsx
+++ src/pages/OmsMembership.jsx
@@ -125,12 +125,12 @@ const OmsMembership = () => {
     }
   };
   return (
-    <div className=" bg-[#141415] plus-jakarta-sans" style={{ backgroundImage: `url(${BG5})` }}>
+    <div className="bg-no-repeat bg-[#141415] plus-jakarta-sans" style={{ backgroundImage: `url(${BG5})` }}>
       {/* <OmsMemebershipCard handleOnPaymentSend={handleOnSubscribe} /> */}
       <div className="font-">
         <section className=" bg-cover bg-center flex flex-col" style={{}}>
           {/* <Nav/> */}
-          <hr className="" />
+        
           <Hero />
         </section>
         <section
@@ -140,7 +140,7 @@ const OmsMembership = () => {
           <Demo />
         </section>
         <section
-          className="bg-cover flex flex-col bg-no-repeat bg-end"
+          className="   "
         >
           <Subscription />
         </section>
