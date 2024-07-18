0e52d3f01d164f8a039fa4e7a39f0a2042f4484f
Dagaroth3exe <buzybeingflmaboyant@gmail.com>
2024-07-18 11:59:31 +0530

    added pricing and card names

--- src/components/memeship-page-component/Subscription.jsx
+++ src/components/memeship-page-component/Subscription.jsx
@@ -1,3 +1,5 @@
+
+
 const Subscription = () => {
   return (
     <>
@@ -8,35 +10,49 @@ const Subscription = () => {
         <section className="flex justify-center">
           <div className="w-full h-auto space-x-1 bg-white rounded-2xl flex justify-evenly items-center py-8 mb-8">
 
-            <div className="h-[440px] w-[400px] bg-[#141415] rounded-xl shadow-2xl ">
+            <div className="h-[470px] w-[400px] bg-[#141415] rounded-xl shadow-2xl ">
 
               <section>
 
-                <div>
-                  <h1 className="text-4xl font-bold text-white pt-5">Basic</h1>
+                <div className="flex flex-col">
+                  <h1 className="text-start text-3xl font-bold text-white pl-6 pt-4">Basic</h1>
+                  <h1 className="flex text-start text-[44px] font-bold text-white pt-1 pl-6">₹1000<p className="font-thin">/mo</p></h1>
+                  
+                  <div className="w-[280px] h-[230px] bg-white rounded-2xl "></div>
+
                 </div>
 
               </section>
 
             </div>
 
-            <div className="h-[440px] w-[400px] bg-[#141415] rounded-xl shadow-2xl ">
+            <div className="h-[470px] w-[400px] bg-[#141415] rounded-xl shadow-2xl ">
               <section>
 
-                <div>
-                  <h1 className="text-4xl font-bold text-white pt-5">Professional</h1>
+              <div className="flex flex-col">
+                  <h1 className="text-start text-3xl font-bold text-white pl-6 pt-4">Basic</h1>
+                  <h1 className="flex text-start text-[44px] font-bold text-white pt-1 pl-6">₹1000<p className="font-thin">/mo</p></h1>
+                  
+                  <div className="w-[280px] h-[230px] bg-white rounded-2xl "></div>
+
                 </div>
+
 
               </section>
             </div>
 
-            <div className="h-[440px] w-[400px] bg-[#141415] rounded-xl shadow-2xl ">
+            <div className="h-[470px] w-[400px] bg-[#141415] rounded-xl shadow-2xl ">
               
               <section>
 
-                <div>
-                  <h1 className="text-4xl font-bold text-white pt-5">Enterprise</h1>
+              <div className="flex flex-col">
+                  <h1 className="text-start text-3xl font-bold text-white pl-6 pt-4">Basic</h1>
+                  <h1 className="flex text-start text-[44px] font-bold text-white pt-1 pl-6">₹1000<p className="font-thin">/mo</p></h1>
+                  
+                  <div className="w-[280px] h-[230px] bg-white rounded-2xl "></div>
+
                 </div>
+
 
               </section>
 
