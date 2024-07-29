dad9eb50418dd4dae3d30dbabee19525ff80e18f
Dagaroth3exe <buzybeingflmaboyant@gmail.com>
2024-07-29 13:42:06 +0530

    added hover effect to blocks

---
+++ src/pages/Blog.jsx
@@ -0,0 +1,18 @@
+import BG5 from "/assets/path2.svg";
+
+const Blg =()=>{
+    return(
+        <div className="bg-no-repeat bg-[#141415] plus-jakarta-sans h-auto px-10 md:px-24 "
+        style={{ backgroundImage: `url(${BG5})` }} >
+
+            <section>
+
+                <div className="h-"></div>
+
+            </section>
+
+        </div>
+    )
+}
+
+export default Blg;
